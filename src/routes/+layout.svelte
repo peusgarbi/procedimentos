72d90728db075aa3b695c4cd804dd8f78dabc9e5
Pedro Sgarbi <peusgarbi@gmail.com>
2024-02-28 04:07:11 -0300

    feature(nav-bar)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
+	import { themeChange } from "theme-change";
+	import { onMount } from "svelte";
 	import "../app.css";
+
+	onMount(() => {
+		themeChange(false);
+	});
 </script>
 
 <slot />

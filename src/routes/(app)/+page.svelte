f4c624cdeb650147dd7938cfce3eb75edf678eff
Pedro Sgarbi <peusgarbi@gmail.com>
2024-02-28 07:53:20 -0300

    feature(home-hero)

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,13 +1,11 @@
 <main>
-	<div class="w-full max-w-xl mx-auto">
-		<h1>Welcome to SvelteKit</h1>
-		<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-		<form action="/auth/logout" method="POST">
-			<button
-				type="submit"
-				class="daisy-btn daisy-btn-xs sm:daisy-btn-sm md:daisy-btn-md lg:daisy-btn-lg"
-				>Logout</button
-			>
-		</form>
+	<div class="daisy-hero h-full bg-base-200 rounded-lg drop-shadow-md p-10">
+		<div class="daisy-hero-content text-center">
+			<div class="max-w-md">
+				<h1 class="text-5xl font-bold">Seja bem-vindo(a)!</h1>
+				<p class="py-6">Aproveite os sistemas para ajudar sua vida!</p>
+				<a href="/ponto" class="daisy-btn daisy-btn-primary">Bater Ponto</a>
+			</div>
+		</div>
 	</div>
 </main>
